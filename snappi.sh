06ad1bf49f5836c03674902cf1f99520cf1fb7be
Dan Bishop <d@nbishop.uk>
2023-06-09 21:58:39 +0100

    test

--- snappi.sh
+++ snappi.sh
@@ -17,9 +17,43 @@ apt-get dist-upgrade -y
 # Start reboot at 05:45 every day (give 5 minute warning to anyone logged in)
 ( crontab -l | grep -v -F "/sbin/shutdown -r +5" || : ; echo "45 5   *   *   *    /sbin/shutdown -r +5" ) | crontab -
 
-# Downlaods and install snapclient
+# Download and install snapclient
 wget https://github.com/badaix/snapcast/releases/download/v0.27.0/snapclient_0.27.0-1_armhf.deb -O /tmp/snapclient.deb
 apt-get install -y /tmp/snapclient.deb
+
+# Power Optimisations
+# Disable HDMI
+sudo /opt/vc/bin/tvservice -o
+# Disable BT and WiFi
+grep -qxF 'dtoverlay=disable-bt' /boot/config.txt || echo 'dtoverlay=disable-bt' >> /boot/config.txt
+grep -qxF 'dtoverlay=disable-wifi' /boot/config.txt || echo 'dtoverlay=disable-wifi' >> /boot/config.txt
+# Pi specific savings
+case raspi-config nonint get_pi_type in
+
+  0)
+    echo 'zero'
+    ;;
+
+  1)
+    echo 'one'
+    ;;
+
+  2)
+    echo '2'
+    ;;
+
+  3)
+    echo 'three'
+    ;;
+  
+  4)
+    echo 'four'
+    ;;
+esac
+
+# Disable LEDs
+grep -qxF 'dtparam=act_led_trigger=none' /boot/config.txt || echo 'dtparam=act_led_trigger=none' >> /boot/config.txt
+grep -qxF 'dtparam=act_led_activelow=on' /boot/config.txt || echo 'dtparam=act_led_activelow=on' >> /boot/config.txt
 
 # Make filesystem readonly
 raspi-config nonint enable_overlayfs
